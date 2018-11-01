bf9c969c7d1405576dada9f761fe687f5b80e80a
Bassett James B <BassettJB@mukilteo.wednet.edu>
2018-11-01 16:37:26 -0700

    Completed Storing.cs

--- CodeXX101/Program.cs
+++ CodeXX101/Program.cs
@@ -11,7 +11,7 @@ namespace CodeXX101
         static void Main(string[] args)
         {
             Storing.Initialize();
-            Storing.DemoVariables();
+            Storing.ConstantsEnumsStructs();
 
 
             Console.WriteLine("Press ANY Key to EXIT.");
