7b997be44cfc202077252a4b343ecdad6f2723eb
Bassett James B <BassettJB@mukilteo.wednet.edu>
2018-11-01 13:48:37 -0700

    Initial Push

---
+++ CodeXX101/Storing.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeXX101
+{
+    public static class Storing
+    {
+
+        public static void Initialize()
+        {
+            Console.WriteLine("Storing Class Initialized.");
+            Console.Title = "Initializer";
+        }
+
+        private static void SetTitles(string title)
+        {
+            Console.Title = title;
+            Console.WriteLine(title + "\n-------------------------\n");
+        }
+
+        public static void DemoVariables()
+        {
+            SetTitles("Demo Variables");
+            //Variable types: 
+            //int, uint, char, float, double, decimal, bool, string
+
+            int number = -1;
+            uint positiveNumber = 2;
+            char letter = 'Z';
+            float body = 98.6f;
+
+            
+        
+        }
+
+        public static void ReadingInput()
+        {
+            //How to read input from the console
+            SetTitles("Reading Input");
+
+            //Using Convert.To___
+
+        }
+
+        public static void UsingArrays()
+        {
+            //How to read input from the console
+            SetTitles( "Using Arrays");
+
+            //Simple 1D arrays
+
+            //2D arrays
+
+            //Casting Data Types
+
+        }
+
+        public static void ConstantsEnumsStructs()
+        {
+            SetTitles( "Constants, Enums, and Structs");
+
+            //How to use an enum
+
+
+            //How to use a constant
+
+            
+            //How to use a struct
+            
+         
+
+        }
+
+    }
+}
